722764e787d5e0f426464a0ec7474a5cc809914b
Ken Abe <kenabe.talent@gmail.com>
2024-08-22 12:56:48 -0800

    Update backend as vercel serverless

---
+++ api/index.ts
@@ -0,0 +1,53 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import OpenAi from 'openai';
+
+const server = express();
+
+const aiClient = new OpenAi({
+  apiKey: process.env.GPT_API_KEY
+});
+
+server.use(cors());
+server.use(express.json());
+
+server.post('/api/chat', async (req, res) => {
+  const userMessage = req.body.message;
+
+  try {
+    const stream = await aiClient.chat.completions.create({
+      model: 'gpt-4o',
+      messages: [
+        { role: 'system', content: 'Reply to user message with proper answer.' },
+        { role: 'user', content: userMessage }
+      ],
+      stream: true
+    });
+    res.setHeader('Content-Type', 'text/plain');
+
+    for await (const chunk of stream) {
+      const content = chunk.choices[0]?.delta?.content || '';
+      res.write(content);
+    }
+
+    res.end();
+  } catch (err) {
+    console.error('Error streaming response:', JSON.stringify(err));
+
+    if (!res.headersSent) {
+      res.status(500).json({
+        error: 'Failed to retrieve a response from the LLM. Please try again later.'
+      });
+    } else {
+      res.write('\n[ERROR]: Something went wrong. Please try again later.');
+      res.end();
+    }
+  }
+});
+
+module.exports = (req: Request, res: Response) => {
+  return server(req, res);
+};
